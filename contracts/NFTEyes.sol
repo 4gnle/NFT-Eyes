282b0a27332d30cf1140c928540c5aacb3b5135b
Angel <angel333gomez@gmail.com>
2021-10-17 10:26:25 -0300

    Contract Update

--- contracts/NFTEyes.sol
+++ contracts/NFTEyes.sol
@@ -10,17 +10,58 @@ contract NFTEyes is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+  string[] firstWords = ["Yes", "No", "Maybe", "Perhaps", "Absolutely", "You Crazy?"];
+
+  string[] secondWords = ["DoIT!", "Avoid!", "GetAway!", "Think!", "Careful!", "OfCourse!"];
+
+  string[] thirdWords = ["NoBalls!", "Pussy!", "Moron!", "Dumbass!", "King!", "Ser!"];
+
   constructor() ERC721 ("NFT EYES", "EYE") {
     console.log("This is my the first EYE NFT contract. YAY!");
+  }
+
+  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+
+  uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+
+  rand = rand % firstWords.length;
+  return firstWords[rand];
+  }
+
+  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+    rand = rand % secondWords.length;
+    return secondWords[rand];
+  }
+
+  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+    rand = rand % thirdWords.length;
+    return thirdWords[rand];
+  }
+
+  function random(string memory input) internal pure returns (uint256) {
+      return uint256(keccak256(abi.encodePacked(input)));
   }
 
   function makeTheNFT() public {
 
     uint256 newItemId = _tokenIds.current();
 
+    string memory first = pickRandomFirstWord(newItemId);
+    string memory second = pickRandomSecondWord(newItemId);
+    string memory third = pickRandomThirdWord(newItemId);
+
+    string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+    console.log("\n--------------------");
+    console.log(finalSvg);
+    console.log("--------------------\n");
+
     _safeMint(msg.sender, newItemId);
 
-    _setTokenURI(newItemId, "An NFT Collection of Drawn Eyes");
+    _setTokenURI(newItemId, "blah!");
 
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
